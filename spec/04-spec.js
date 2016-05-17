a7b58b4a67622be7f90ace5f61d435af5bba631c
metakata <menning23@gmail.com>
2016-05-16 23:45:29 -0400

    partial list of included matchers

---
+++ spec/04-spec.js
@@ -0,0 +1,54 @@
+describe("Included matchers:", function(){
+	it("The 'toBe' matcher compares with ===", function(){
+		var a = 12;
+		var b = a;
+		
+		expect(a).toBe(b);
+		expect(a).not.toBe(null);
+	});
+	
+	describe("The 'toEqual' matcher",function(){
+		it("works for simple literals and variables", function(){
+			var a = 12;
+			expect(a).toEqual(12);
+		});
+		
+		it("should work for objects", function(){
+			var foo = {
+				a: 12,
+				b: 34
+			};
+			var bar = {
+				a: 12,
+				b: 34
+			};
+			expect(foo).toEqual(bar);
+		});
+	});
+	
+	it("The 'toMatch' matcher is for regular expressions", function(){
+		var message = 'foo bar baz';
+		
+		expect(message).toMatch(/bar/);
+		expect(message).toMatch('bar');
+		expect(message).not.toMatch(/quux/);
+	});
+	
+	it("The 'toBeDefined' matcher compares against 'undefined'", function(){
+		var a = {
+			foo: 'foo'
+		};
+		
+		expect(a.foo).toBeDefined();
+		expect(a.bar).not.toBeDefined();
+	});
+	
+	it("The 'toBeUndefined' matcher compares against 'undefined'", function(){
+		var a = {
+			foo: 'foo'
+		};
+		
+		expect(a.foo).not.toBeUndefined();
+		expect(a.bar).toBeUndefined();
+	});
+});
