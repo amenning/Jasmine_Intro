dbbfb7a3fb922a19a143e1bcf5f8d4ece83889f3
metakata <menning23@gmail.com>
2016-05-16 23:36:55 -0400

    not toBe example

--- spec/03-spec.js
+++ spec/03-spec.js
@@ -2,4 +2,8 @@ describe("The 'toBe' matcher compares with ===", function(){
 	it("and has a positive case", function(){
 		expect(true).toBe(true);
 	});
+	
+	it("and can have a negative case", function(){
+		expect(false).not.toBe(true);
+	});
 });
