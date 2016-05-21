b64040dece75e99ce738a015dfca9574407d1c5f
metakata <menning23@gmail.com>
2016-05-21 16:14:04 -0400

    spy callThrough example

---
+++ spec/14-spec.js
@@ -0,0 +1,66 @@
+/* Spies: and.callThrough
+
+By chaining the spy with and.callThrough, the spy will still track 
+all calls to it but in addition it will delegate to the actual 
+implementation.
+*/
+describe("A spy, when configured to call through", function(){
+	var foo, bar, fetchedBar;
+ 
+	beforeEach(function(){
+		foo = {
+			setBar: function(value){
+				bar = value;
+			},
+			getBar: function(){
+				return bar;
+			}
+		};
+		
+		spyOn(foo, 'getBar').and.callThrough();
+		
+		foo.setBar(123);
+		fetchedBar = foo.getBar();
+	});
+	
+	it("tracks that the spy was called", function(){
+		expect(foo.getBar).toHaveBeenCalled();
+	});
+	
+	it("should not affect other functions", function(){
+		expect(bar).toEqual(123);
+	});
+	
+	it("when called returns the requested value", function(){
+		expect(fetchedBar).toEqual(123);
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
