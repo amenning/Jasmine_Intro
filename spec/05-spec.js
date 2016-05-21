fced21e4a8a5371262b4c9719fad095b1495774e
metakata <menning23@gmail.com>
2016-05-21 11:25:20 -0400

    fail function example

--- spec/05-spec.js
+++ spec/05-spec.js
@@ -1,44 +1,14 @@
-var customMatchers = {
-	toBeGoofy: function(util, customEqualityTesters){
-		return {
-			compare: function(actual, expected){
-				if(expected === undefined){
-					expected = '';
-				}
-				var result = {};
-				result.pass = util.equals(actual.hyuk, "gawrsh" + expected, customEqualityTesters);
-				if (result.pass){
-					result.message = "Expected " + actual + " not to be quite so goofy";
-				}else{
-					result.message = "Expected " + actual + " to be goofy, but it was not very goofy";
-				}
-				return result;
-			}
-		};
-	}
-};
-
-describe("Custom matcher: 'toBeGoofy'", function(){
-	beforeEach(function(){
-		jasmine.addMatchers(customMatchers);
-	});
+describe("A spec using the fail function", function(){
+	var foo = function(x, callBack){
+		if(x){
+			callBack();
+		}
+	};
 	
-	it("is available on an expectation", function(){
-		expect({
-			hyuk: 'gawrsh'
-		}).toBeGoofy();
-	});
-	
-	it("can take an 'expected' parameter", function(){
-		expect({
-			hyuk: 'gawrsh is fun'
-		}).toBeGoofy(' is fun');
-	});
-	
-	it("can be negated", function(){
-		expect({
-			hyuk: 'this is fun'
-		}).not.toBeGoofy();
+	it("should not call the callback", function(){
+		foo(false, function(){
+			fail("Callback has been called");
+		});
 	});
 });
 
